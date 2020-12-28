e0a922273829c22fb3835e1be01c5052fb53a8d4
Varun Kumaar <teralad@gmail.com>
2020-12-28 22:51:08 +0530

    add booking table

--- db/migrate/20201228163556_create_events.rb
+++ db/migrate/20201228163556_create_events.rb
@@ -1,7 +1,12 @@
 class CreateEvents < ActiveRecord::Migration[6.0]
   def change
     create_table :events do |t|
-
+      t.string    :title
+      t.datetime  :start_time
+      t.datetime  :end_time
+      t.text      :description
+      t.boolean   :all_day
+      t.integer   :status
       t.timestamps
     end
   end
--- db/migrate/20201228163608_create_users.rb
+++ db/migrate/20201228163608_create_users.rb
@@ -1,7 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-
+      t.string    :username, unique: true
+      t.string    :email
+      t.string    :phone
+      t.boolean   :archived, default: false
       t.timestamps
     end
   end
