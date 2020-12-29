7a0b9e4cb31a4f9783afc6468b23351ad9107489
Varun Kumaar <teralad@gmail.com>
2020-12-30 00:17:09 +0530

    migrations for booking table and schema changes

--- db/migrate/20201228164722_create_bookings.rb
+++ db/migrate/20201228164722_create_bookings.rb
@@ -1,8 +1,12 @@
 class CreateBookings < ActiveRecord::Migration[6.0]
   def change
     create_table :bookings do |t|
-
+      t.references :event
+      t.references :user
+      t.integer    :inviter
+      t.integer    :status
       t.timestamps
     end
+    add_index :bookings, [:event_id, :user_id], unique: true
   end
 end
