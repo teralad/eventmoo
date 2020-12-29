6c7d9f403133fbeb1ccc25ddf34bca466710529c
Varun Kumaar <teralad@gmail.com>
2020-12-29 21:12:45 +0530

    script to seed user and user model validations

---
+++ db/default/database_helper.rb
@@ -0,0 +1,16 @@
+CHUNK_SIZE = 1000
+module DatabaseHelper
+
+  def import_records(model_name: , records:)
+    model = model_name.constantize
+    begin
+      model.import! records
+    rescue ActiveRecord::RecordInvalid => e
+      puts "Inserting valid values only since #{e}"
+      model.import records
+    rescue ActiveRecord::RecordNotUnique => e
+      puts "Ignoring as record is already present"
+    end
+  end
+
+end
--- db/default/eventmoo/user.rb
+++ db/default/eventmoo/user.rb
@@ -1,12 +1,38 @@
 require 'activerecord-import'
 require 'csv'
 
-user_csv_path = Rails.root.join('lib', 'seed_files', 'users.csv')
-users = []
-CSV.foreach(user_csv_path) do |row|
-  users << User.new do |user|
-    user.username = row[0]
-    user.email    = row[1]
-    user.phone    = row[2]
+class UserRow
+  def initialize(data)
+    @username = data[0]
+    @email    = data[1]
+    @phone    = data[2]
+  end
+
+  def to_h
+    {
+      username: @username,
+      email:    @email,
+      phone:    clean_phone
+    }
+  end
+
+  private
+
+  def clean_phone
+    @phone = @phone.gsub(/[^0-9A-Za-z]/, '')[/[^Xx]+/]
   end
 end
+
+include DatabaseHelper
+
+user_csv_path = Rails.root.join('lib', 'seed_files', 'users.csv')
+users = []
+CSV.foreach(user_csv_path, headers: true) do |row|
+  user_data = UserRow.new(row).to_h
+  users << User.new(user_data)
+  if users.length >= CHUNK_SIZE
+    import_records(model_name: User.name, records: users)
+    users.clear
+  end
+end
+import_records(model_name: User.name, records: users)
