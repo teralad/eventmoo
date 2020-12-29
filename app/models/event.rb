6cdeb425fc118154ca97c73ba3e252930bdc5de5
Varun Kumaar <teralad@gmail.com>
2020-12-30 00:13:29 +0530

    class variable

--- app/models/event.rb
+++ app/models/event.rb
@@ -13,7 +13,7 @@ class Event < ApplicationRecord
   def mark_complete_if_end_date_in_past
     if (end_time.present? && end_time.to_datetime < Time.now.to_datetime) ||
       (all_day && start_time.present? && start_time.to_date < Date.today)
-      status = statuses[:completed]
+      status = self.class.statuses[:completed]
     end
   end
 end
