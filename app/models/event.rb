eefc12252226051eb926c5f2d281ff2ec4c6eb29
Varun Kumaar <teralad@gmail.com>
2020-12-30 16:56:20 +0530

    added model validations for newer inserts

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,6 +7,7 @@ class Event < ApplicationRecord
   has_many :bookings
 
   validates_presence_of :start_time
+  validates_presence_of :end_time, if: proc { |event| !event.all_day }
 
   before_create :mark_complete_if_end_date_in_past
 
@@ -14,6 +15,14 @@ class Event < ApplicationRecord
     if (end_time.present? && end_time.to_datetime < Time.now.to_datetime) ||
       (all_day && start_time.present? && start_time.to_date < Date.today)
       status = self.class.statuses[:completed]
+    end
+  end
+
+  def get_end_time
+    if self.end_time.present?
+      self.end_time
+    elsif self.all_day
+      self.start_time.end_of_day
     end
   end
 end
