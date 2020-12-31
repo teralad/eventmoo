b40d1f9857cfdbda22b7e550f02b16a5aa765146
Varun Kumaar <teralad@gmail.com>
2020-12-31 11:31:31 +0530

    root to home page by default

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: redirect('/home')
+  get '/home', to: 'application#home'
+
   resources :users, only: [:index, :show]
   resources :events, only: [:index]
   resources :bookings
