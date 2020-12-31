672315121cd269a6b89c33b35cc9b0296035eb83
Varun Kumaar <teralad@gmail.com>
2020-12-31 11:32:26 +0530

    home page controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
 
+
+  def home
+    render 'layouts/home'
+  end
+
   def set_range
     @range = DatetimeRange.new(params[:start_time], params[:end_time])
   end
